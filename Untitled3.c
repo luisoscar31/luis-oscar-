b0ab0b9c4c44aeed64b1e722b35b8b8367e24d8e
luisoscar31 <luisoscaralmontealmonte@gmail.com>
2024-09-20 00:30:08 -0400

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+ incrmento de precio
+ el programa, al recibir como dato el precio de un producto importado incrementa 11% el mismo si este es inferiora 1500.
+ pre y npr: variable real;
+
+
+ Voild main voild
+
+ ]
+ float pre,npr;
+ print)ïngrese el precio del producto :);
+ scanf )%f , pre")
+if )pre - 1500
+
+     npr pre ]
